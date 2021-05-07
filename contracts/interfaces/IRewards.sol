19f8312ddb3130e110b68a25d2b847a46602b544
Pylyp Radionov <p.radionov@4irelabs.com>
2021-05-07 11:01:57 +0300

    feat: rewards interface
    * removed internal methods
    * set explicitly solc version

--- contracts/interfaces/IRewards.sol
+++ contracts/interfaces/IRewards.sol
@@ -1,19 +1,14 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.4;
 
 interface IRewards {
     event NewClaim(uint256 amount, address token, address to);
 
     function stake(address _nftToken) external;
 
-    function ustake(address _nftToken) external;
+    function unstake(address _nftToken) external;
 
-    function claim(address _to, uint256 _amount) external payable;
+    function claim() external;
 
     function getReward() external view;
 
-    function claimETH(address _to, uint256 _amount) external payable;
-
-    function getRewardETH() external view;
-
-    function recalculate() external;
 }
