97f1065251ba619db24b55f781ff0a23cd82f28e
Viacheslav Gonkivskyi <vyacheslav.g@4irelabs.com>
2021-05-08 22:52:41 +0300

    feat: add script for testing contract on mainnet

--- contracts/interfaces/IRewards.sol
+++ contracts/interfaces/IRewards.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.5;
 interface IRewards {
     event NewClaim(uint256 _amount, address _token, address _to);
 
-    function stake(address _nftToken, uint256 _id) external;
+    function stake(uint256 _id) external view returns (uint128);
 
     function unstake(address _nftToken) external;
 
