0d4dd5a646d0dc588572f755aa9f7d8306749f60
Pylyp Radionov <p.radionov@4irelabs.com>
2021-05-14 15:07:16 +0300

    feat: lint

--- contracts/interfaces/IRewards.sol
+++ contracts/interfaces/IRewards.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.7.5;
 
 interface IRewards {
-    event NewClaim(uint256 _amount, address _token, address _to);
-    event NewStake(uint256 _tokenID);
+  event NewClaim(uint256 _amount, address _token, address _to);
+  event NewStake(uint256 _tokenID);
 
-    function stake(uint256 _id) external payable returns (uint128);
+  function stake(uint256 _id) external payable returns (uint128);
 
-    function unstake(address _nftToken) external;
+  function unstake(address _nftToken) external;
 
-    function claim() external;
+  function claim() external;
 
-    function getReward() external view;
+  function getReward() external view;
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,29 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-typechain';
-import { MAINNET_URL_ALCHEMY } from './wallet';
 import { HardhatUserConfig } from 'hardhat/types';
 
+const MAINNET_URL_ALCHEMY = '';
 const config: HardhatUserConfig = {
-    networks: {
-        hardhat: {
-            forking: {
-                url: MAINNET_URL_ALCHEMY,
-            },
+  networks: {
+    hardhat: {
+      forking: {
+        url: MAINNET_URL_ALCHEMY,
+      },
+    },
+  },
+  solidity: {
+    version: '0.7.5',
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['storageLayout'],
         },
+      },
     },
-    solidity: {
-        version: '0.7.5',
-        settings: {
-            outputSelection: {
-                '*': {
-                    '*': ['storageLayout'],
-                },
-            },
-        },
-    },
-    mocha: {
-        bail: true,
-    },
+  },
+  mocha: {
+    bail: true,
+  },
 };
 export default config;
