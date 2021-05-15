98c69bbaf334186a1f2c57be39237d5890b848b1
Viacheslav Gonkivskyi <vyacheslav.g@4irelabs.com>
2021-05-15 14:54:47 +0300

    fix: rework tests according to comments

--- test/unit-tests/lp-get-user-tokens.ts
+++ test/unit-tests/lp-get-user-tokens.ts
@@ -3,6 +3,8 @@ import { ethers, providers } from 'hardhat';
 import { expect } from 'chai';
 import { Rewards } from '../../typechain/Rewards';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { TX_RECEIPT_OK } from '../../constants/tx-status';
+import { UNISWAP_V3_FACTORY } from '../../constants/uniswaps';
 import {
   deployMockContract,
   MockContract,
@@ -22,7 +24,6 @@ describe('Lp get tokens test suite', () => {
   before('deploy lp contract', async () => {
     //TODO: delete after this values will be added into constants
     uniswapMock = await deployMockContract(uniswap, UNI.abi);
-    const UNISWAP_V3_FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
     const Rewards = await ethers.getContractFactory('Rewards');
     rewards = (await Rewards.connect(deployer).deploy(
       UNISWAP_V3_FACTORY,
@@ -32,11 +33,7 @@ describe('Lp get tokens test suite', () => {
     rewDeployTx = await rewards.deployTransaction.wait();
   });
 
-  it('lp OK deploy', async () => {
-    expect(rewDeployTx.status).eq(1);
-  });
-
-  it('stake tokens', async () => {
+  before('mock uniswap', async () => {
     await uniswapMock.mock.positions.returns(
       0,
       user.address,
@@ -53,23 +50,29 @@ describe('Lp get tokens test suite', () => {
     );
     await uniswapMock.mock.getApproved.returns(rewards.address);
     await uniswapMock.mock.transferFrom.returns();
-    let tx = await rewards.connect(user).stake(666);
+  });
+
+  it('contract deployed sucessfully', async () => {
+    expect(rewDeployTx.status).eq(TX_RECEIPT_OK);
+  });
+
+  it('NewStake emited after stake', async () => {
+    let tx = await rewards.connect(user).stake(1);
     let txr = await tx.wait();
     const expectedEventName =
       rewards.interface.events['NewStake(uint256)'].name;
     expect(txr.events[0].event).eq(expectedEventName);
   });
 
-  it('get user tokens', async () => {
-    await rewards.connect(user).stake(777);
-    expect((await rewards.connect(user).getUserTokens()).toString()).eq(
-      '666,777'
-    );
+  it('returns array of 2 elements [1,2] after getUserTokens', async () => {
+    await rewards.connect(user).stake(2);
+    expect((await rewards.connect(user).getUserTokens()).toString()).eq('1,2');
     expect((await rewards.connect(user).getUserTokens()).length).eq(2);
   });
 
-  it('get user tokens while empty', async () => {
-    expect((await rewards.connect(deployer).getUserTokens()).toString()).eq('');
-    expect((await rewards.connect(deployer).getUserTokens()).length).eq(0);
+  it('returns empty array for getTokens', async () => {
+    let tokens = await rewards.connect(deployer).getUserTokens();
+    expect(tokens.toString()).eq('');
+    expect(tokens.length).eq(0);
   });
 });
