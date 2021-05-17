cbd0230430372ea15e76be9c4287b683532ce9db
a-sandyuk <artem.s@4irelabs.com>
2021-05-17 19:52:30 +0300

    fix: node reset

--- test/unit-tests/lp-receive-eth.test.ts
+++ test/unit-tests/lp-receive-eth.test.ts
@@ -53,6 +53,15 @@ describe('Lp receive ETH test suite', () => {
   after('reset node fork', async () => {
     await network.provider.request({
       method: 'hardhat_reset',
+      params: [
+        {
+          forking: {
+            jsonRpcUrl:
+              'https://eth-mainnet.alchemyapi.io/v2/iHddcEw1BVe03s2BXSQx_r_BTDE-jDxB',
+            blockNumber: 12419631,
+          },
+        },
+      ],
     });
   });
 });
