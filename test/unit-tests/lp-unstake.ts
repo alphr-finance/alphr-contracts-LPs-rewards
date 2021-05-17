8f8be905bac1a2f1c532ae00c9cd191b1ee95a40
Pylyp Radionov <p.radionov@4irelabs.com>
2021-05-17 15:01:18 +0300

    fix: refactor test

--- test/unit-tests/lp-unstake.ts
+++ test/unit-tests/lp-unstake.ts
@@ -2,7 +2,7 @@
 //@ts-ignore
 import { ethers, providers } from 'hardhat';
 import { expect } from 'chai';
-import { Rewards } from '../../typechain/Rewards';
+import { Rewards } from '../../typechain';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { TX_RECEIPT_OK } from '../../constants/tx-status';
 import { UNISWAP_V3_FACTORY } from '../../constants/uniswaps';
@@ -10,21 +10,26 @@ import {
   deployMockContract,
   MockContract,
 } from '@ethereum-waffle/mock-contract';
+import * as uniswapNftPosistionManager from '../../artifacts/@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json';
 
-const UNI = require('../../artifacts/@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json');
-
-describe('Lp get tokens test suite', () => {
+describe('LPs Rewards ::  unstake method test suite', () => {
   let deployer, uniswap, user: SignerWithAddress;
-  let rewards: Rewards;
-  let uniswapMock: MockContract;
-  let rewDeployTx: providers.TransactionReceipt;
 
   before('init signers', async () => {
     [deployer, uniswap, user] = await ethers.getSigners();
   });
-  before('deploy lp contract', async () => {
-    //TODO: delete after this values will be added into constants
-    uniswapMock = await deployMockContract(uniswap, UNI.abi);
+
+  let uniswapMock: MockContract;
+  before('deploy uniswap mock contract', async () => {
+    uniswapMock = await deployMockContract(
+      uniswap,
+      uniswapNftPosistionManager.abi
+    );
+  });
+
+  let rewards: Rewards;
+  let rewDeployTx: providers.TransactionReceipt;
+  before('deploy lp rewards contract', async () => {
     const Rewards = await ethers.getContractFactory('Rewards');
     rewards = (await Rewards.connect(deployer).deploy(
       UNISWAP_V3_FACTORY,
@@ -33,12 +38,18 @@ describe('Lp get tokens test suite', () => {
     await rewards.deployed();
     rewDeployTx = await rewards.deployTransaction.wait();
   });
+
+  it('has correct receipt status of rewards contract deploy', async () => {
+    expect(rewDeployTx.status).to.eq(TX_RECEIPT_OK);
+  });
+
   before('mock uniswap', async () => {
+    // https://docs.uniswap.org/reference/periphery/NonfungiblePositionManager#positions
     await uniswapMock.mock.positions.returns(
       0,
-      user.address,
-      user.address,
-      user.address,
+      user.address, // operator
+      '0xaa99199d1e9644b588796f3215089878440d58e0', // ALPHR
+      '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
       0,
       0,
       0,
@@ -51,25 +62,22 @@ describe('Lp get tokens test suite', () => {
     await uniswapMock.mock.getApproved.returns(rewards.address);
     await uniswapMock.mock.transferFrom.returns();
   });
-  it('lp OK deploy', async () => {
-    expect(rewDeployTx.status).to.eq(TX_RECEIPT_OK);
-  });
 
-  it('reverted unstake when user does not have staked  tokens', async () => {
-    await expect(rewards.connect(user).unstake(1)).to.be.revertedWith(
+  it('reverts unstake tx if user does not have staked tokens', async () => {
+    await expect(rewards.connect(user).unstake('1')).to.be.revertedWith(
       'User must have staked tokens'
     );
   });
 
-  it('revert when trying to stake token that is not owned by user', async () => {
-    await rewards.connect(user).stake(1);
-    await expect(rewards.connect(user).unstake(2)).to.be.revertedWith(
+  it('reverts unstake tx if token is not owned by user', async () => {
+    await rewards.connect(user).stake('1');
+    await expect(rewards.connect(user).unstake('2')).to.be.revertedWith(
       'User must owned this token'
     );
   });
 
-  it('NewUnstake emitted during unstake', async () => {
-    const tx = await rewards.connect(user).unstake(1);
+  it('emits correct event in unstake tx', async () => {
+    const tx = await rewards.connect(user).unstake('1');
     const txr = await tx.wait();
     const expectedEventName =
       rewards.interface.events['NewUnstake(uint256,address)'].name;
