b551d7c3ae3579ebe3881efc520d82751ee8c4d2
a-sandyuk <artem.s@4irelabs.com>
2021-05-17 16:20:47 +0300

    feat: add unit test

---
+++ test/unit-tests/lp-receive-eth.test.ts
@@ -0,0 +1,58 @@
+/* eslint-disable jest/valid-expect */
+//@ts-ignore
+import { network, ethers, providers } from 'hardhat';
+import { expect } from 'chai';
+import { Rewards } from '../../typechain/Rewards';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { TX_RECEIPT_OK } from '../../constants/tx-status';
+import { UNISWAP_V3_FACTORY } from '../../constants/uniswaps';
+import {
+  deployMockContract,
+  MockContract,
+} from '@ethereum-waffle/mock-contract';
+import { utils } from 'ethers';
+
+const UNI = require('../../artifacts/@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json');
+
+describe('Lp receive ETH test suite', () => {
+  let deployer, uniswap, user: SignerWithAddress;
+  let rewards: Rewards;
+  let uniswapMock: MockContract;
+  let rewDeployTx: providers.TransactionReceipt;
+
+  before('init signers', async () => {
+    [deployer, uniswap, user] = await ethers.getSigners();
+  });
+
+  before('deploy lp contract', async () => {
+    //TODO: delete after this values will be added into constants
+    uniswapMock = await deployMockContract(uniswap, UNI.abi);
+    const Rewards = await ethers.getContractFactory('Rewards');
+    rewards = (await Rewards.connect(deployer).deploy(
+      UNISWAP_V3_FACTORY,
+      uniswapMock.address
+    )) as Rewards;
+    await rewards.deployed();
+    rewDeployTx = await rewards.deployTransaction.wait();
+  });
+
+  it('contract deployed sucessfully', async () => {
+    expect(rewDeployTx.status).eq(TX_RECEIPT_OK);
+  });
+
+  it('send 100 ETH to rewards contract and check balance', async () => {
+    await user.sendTransaction({
+      to: rewards.address,
+      value: utils.parseEther('100'),
+    });
+    expect(await ethers.provider.getBalance(rewards.address)).eq(
+      utils.parseEther('100')
+    );
+  });
+
+  after('reset node fork', async () => {
+    await network.provider.request({
+      method: 'hardhat_reset',
+    });
+  });
+});
