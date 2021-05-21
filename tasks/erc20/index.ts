7741a4f463df5fee8c0e967b6665b600bbef69d1
a-sandyuk <artem.s@4irelabs.com>
2021-05-21 15:15:24 +0300

    feat: update hardhat task indexes

---
+++ tasks/erc20/index.ts
@@ -0,0 +1,4 @@
+import * as ERC20MockDeploy from './deploy';
+import * as ERC20MockMint from './mint';
+import * as ERC20MockMintTo from './mint-to';
+export default { ERC20MockDeploy, ERC20MockMint, ERC20MockMintTo };
--- tasks/index.ts
+++ tasks/index.ts
@@ -1,4 +1,5 @@
 import * as UniswapTasks from './uniswap/index';
 import * as RewardsTasks from './rewards/index';
 import * as LPsBootstrap from './lps-bootstrap';
-export { UniswapTasks, RewardsTasks, LPsBootstrap };
+import * as ERC20Tasks from './erc20/index';
+export { UniswapTasks, RewardsTasks, ERC20Tasks, LPsBootstrap };
