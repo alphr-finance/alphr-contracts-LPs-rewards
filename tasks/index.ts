bf457018a90852acd1566ab3c0647e9349d39573
Viacheslav Gonkivskyi <vyacheslav.g@4irelabs.com>
2021-05-21 11:59:59 +0300

    fix: move uniswap to a separate folder

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,3 +1,3 @@
-import * as uni from './uniswap';
-
-export { uni };
+import * as UniswapTasks from './uniswap/uniswap';
+import * as UniswapParams from './uniswap/uniswap-names';
+export { UniswapTasks, UniswapParams };
---
+++ tasks/uniswap/indxex.ts
@@ -0,0 +1,3 @@
+import * as uni from './uniswap';
+
+export { uni };
