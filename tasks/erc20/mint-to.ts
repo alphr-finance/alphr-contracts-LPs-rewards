82b1bf300d9bc2a6811352800a1f9f9fccd61391
a-sandyuk <artem.s@4irelabs.com>
2021-05-21 15:36:27 +0300

    fix: remove typechain imports in tasks

--- tasks/erc20/mint-to.ts
+++ tasks/erc20/mint-to.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { task } from 'hardhat/config';
-import { IERC20 } from '../../typechain';
+//import { IERC20 } from '../../typechain';
 
 task('erc20mock:mintTo', 'erc20 mint token amount to recipient')
   .addParam('address', 'ERC20Mock contract address')
@@ -8,10 +8,10 @@ task('erc20mock:mintTo', 'erc20 mint token amount to recipient')
   .addParam('to', 'recipient address')
   .setAction(async ({ address, amount, to }, hre) => {
     let recipient: SignerWithAddress;
-    let token: IERC20;
+    let token;
     recipient = await hre.ethers.getSigner(to);
 
-    token = (await hre.ethers.getContractAt('ERC20Mock', address)) as IERC20;
+    token = await hre.ethers.getContractAt('ERC20Mock', address);
 
     console.log(
       'Recipient token balance before mint:\t',
--- tasks/erc20/mint.ts
+++ tasks/erc20/mint.ts
@@ -1,16 +1,15 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { task } from 'hardhat/config';
-import { IERC20 } from '../../typechain';
 
 task('erc20mock:mint', 'erc20 mint token amount to msg sender')
   .addParam('address', 'ERC20Mock contract address')
   .addParam('amount', 'token amount to mint')
   .setAction(async ({ address, amount }, hre) => {
     let sender: SignerWithAddress;
-    let token: IERC20;
+    let token;
     [sender] = await hre.ethers.getSigners();
 
-    token = (await hre.ethers.getContractAt('ERC20Mock', address)) as IERC20;
+    token = await hre.ethers.getContractAt('ERC20Mock', address);
 
     console.log(
       'Sender token balance before mint:\t',
