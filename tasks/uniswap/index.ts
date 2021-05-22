aeed49dbe541023bdf0173a7bf76428a8e676932
vgonkivskyi <80709962+vgonkivskyi@users.noreply.github.com>
2021-05-22 12:46:28 +0300

    Feature/alphr383 mint positions (#33)
    * feat: mint position
    
    * fix: add mint into index.ts
    
    * fix: fix rebase
    
    * fix: add mint-names files
    
    * fix: fix linter
    
    * fix: remove unused params

--- tasks/uniswap/index.ts
+++ tasks/uniswap/index.ts
@@ -1,3 +1,5 @@
 import * as UniswapTasks from './create-and-init-pool';
-
-export { UniswapTasks };
+import * as UniswapParams from './create-and-init-pool.names';
+import * as UniswapMint from './mint-positions';
+import * as UniswapMintParams from './mint-position.names';
+export { UniswapTasks, UniswapParams, UniswapMint, UniswapMintParams };
---
+++ tasks/uniswap/mint-positions.ts
@@ -0,0 +1,63 @@
+/* eslint-disable jest/no-standalone-expect */
+/* eslint-disable jest/valid-expect */
+import { task } from 'hardhat/config';
+import { UNISWAP_V3_NFT_POSITION_MANAGER } from './../../constants/uniswaps';
+import { UNISWAP_MINT } from './mint-position.names';
+import { TICK_SPACINGS } from './../../test/shared/constants';
+import { getMinTick, getMaxTick } from './../../test/shared/ticks';
+
+export const MintTask = task('uni:mint', 'mint tokens')
+  .addParam(UNISWAP_MINT.FROM, UNISWAP_MINT.FROM_DESC)
+  .addParam(UNISWAP_MINT.TOKEN0_ADDRESS, UNISWAP_MINT.TOKEN0_ADDRESS_DESC)
+  .addParam(UNISWAP_MINT.TOKEN1_ADDRESS, UNISWAP_MINT.TOKEN1_ADDRESS_DESC)
+  .addParam(UNISWAP_MINT.TICK_LOWER, UNISWAP_MINT.TICK_LOWER_DESC)
+  .addParam(UNISWAP_MINT.TICK_UPPER, UNISWAP_MINT.TICK_UPPER_DESC)
+  .addParam(UNISWAP_MINT.FEE_AMOUNT, UNISWAP_MINT.FEE_AMOUNT_DESC)
+  .addParam(UNISWAP_MINT.RECIPIENT, UNISWAP_MINT.RECIPIENT_DESC)
+  .addParam(UNISWAP_MINT.AMOUNT0_DESIRED, UNISWAP_MINT.AMOUNT0_DESIRED_DESC)
+  .addParam(UNISWAP_MINT.AMOUNT1_DESIRED, UNISWAP_MINT.AMOUNT1_DESIRED_DESC)
+  .addParam(UNISWAP_MINT.AMOUNT0_MIN, UNISWAP_MINT.AMOUNT0_MIN_DESC)
+  .addParam(UNISWAP_MINT.AMOUNT1_MIN, UNISWAP_MINT.AMOUNT1_MIN_DESC)
+  .addParam(UNISWAP_MINT.DEADLINE, UNISWAP_MINT.DEADLINE_DESC)
+  .setAction(
+    async (
+      {
+        from,
+        token0,
+        token1,
+        low,
+        up,
+        fee,
+        recipient,
+        des0,
+        des1,
+        min0,
+        min1,
+        deadline,
+      },
+      hre
+    ) => {
+      const nftManager = await hre.ethers.getContractAt(
+        'INonfungiblePositionManager',
+        UNISWAP_V3_NFT_POSITION_MANAGER
+      );
+      let tx = await nftManager.connect(from).mint(
+        {
+          token0: token0,
+          token1: token1,
+          tickLower: getMinTick(TICK_SPACINGS[low]),
+          tickUpper: getMaxTick(TICK_SPACINGS[up]),
+          fee: fee,
+          recipient: recipient,
+          amount0Desired: des0,
+          amount1Desired: des1,
+          amount0Min: min0,
+          amount1Min: min1,
+          deadline: deadline,
+        },
+        { gasLimit: 12450000 }
+      );
+      let txr = await tx.wait();
+      console.log('Minted tokenId:', txr.events[4].args.tokenId);
+    }
+  );
