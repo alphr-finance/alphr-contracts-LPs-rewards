72ecce070e313967d620bf98883e2a91d70ad001
Viacheslav Gonkivskyi <vyacheslav.g@4irelabs.com>
2021-05-24 12:58:27 +0300

    feat: extend lps-bootstrap

--- tasks/erc20/deploy.ts
+++ tasks/erc20/deploy.ts
@@ -5,11 +5,12 @@ task('erc20mock:deploy', 'deploy erc20 mock contract')
   .addParam('symbol', 'ERC20 token symbol')
   .addParam('decimals', 'ERC20 token decimals')
   .setAction(async ({ name, symbol, decimals }, hre) => {
-    await hre.ethers
+    const address = await hre.ethers
       .getContractFactory('ERC20Mock')
       .then((deployer) => deployer.deploy(name, symbol, decimals))
       .then((token) => token.deployed())
-      .then((rewDeployed) =>
-        console.log('Contracts Address:', rewDeployed.address)
-      );
+      .then((rewDeployed) => {
+        return rewDeployed.address;
+      });
+    return address;
   });
--- tasks/lps-bootstrap.ts
+++ tasks/lps-bootstrap.ts
@@ -1,3 +1,4 @@
+import { utils } from 'ethers';
 import { LP_TEST_BOOTSTRAP } from './lps-bootstrap.names';
 import { LP_DEPLOY } from './rewards/lp-rewards-deploy.names';
 import { task } from 'hardhat/config';
@@ -16,6 +17,51 @@ export default task(LP_TEST_BOOTSTRAP.NAME, LP_TEST_BOOTSTRAP.DESC).setAction(
       nft: UNISWAP_V3_NFT_POSITION_MANAGER,
       alphr: ALPHR_TOKEN,
       pool: ALPHR_UNISWAP_V3_POOL,
+    });
+    const tokenAddress1 = await hre.run('erc20mock:deploy', {
+      name: 'Mock',
+      symbol: 'MK',
+      decimals: '18',
+    });
+    const tokenAddress2 = await hre.run('erc20mock:deploy', {
+      name: 'Mock20',
+      symbol: 'M20',
+      decimals: '18',
+    });
+    console.log(tokenAddress1);
+    console.log(tokenAddress2);
+    await hre.run('erc20mock:mintTo', {
+      address: tokenAddress1,
+      amount: '100',
+      to: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+    });
+    await hre.run('erc20mock:mintTo', {
+      address: tokenAddress2,
+      amount: '100',
+      to: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+    });
+
+    await hre.run('uni:init-pool', {
+      token0: tokenAddress2,
+      token1: tokenAddress1,
+      fee: '3000',
+      sqrtprice: '100',
+    });
+
+    // TODO fix signTransaction issue
+    await hre.run('uni:mint', {
+      from: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+      token0: tokenAddress2,
+      token1: tokenAddress1,
+      low: '3000',
+      up: '3000',
+      fee: '3000',
+      recipient: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+      des0: '100',
+      des1: '200',
+      min0: '1',
+      min1: '1',
+      deadline: utils.parseEther('1').toString(),
     });
   }
 );
