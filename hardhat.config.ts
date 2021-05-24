211301121834d1e434628a8550bff421c18beea8
Artem Sandyuk <80709951+a-sandyuk@users.noreply.github.com>
2021-05-24 12:58:31 +0300

    Feature/alphr 408 typechain fixes and bump (#41)
    * feat: update dependencies
    
    * feat: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
-import 'hardhat-typechain';
+import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import { HardhatUserConfig } from 'hardhat/types';
 const coinmarketcapAPIKey = '53cec9bc-a843-4e72-ab3b-8bf3df2fa87e';
